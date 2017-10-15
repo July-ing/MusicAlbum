7f6b954105c030036a154a87b963363896e07265
July_ing <July_ing279@163.com>
2017-10-15 10:49:44 +0800

    add text

--- public/js/01.js
+++ public/js/01.js
@@ -5,15 +5,14 @@ function parseDom(arg){
 	objE.innerHTML = arg;
 	return objE.childNodes;
 }
-
-oNodeList[0] = parseDom('<li ><div class="content" ><div class="ge15"><h2></h2> <em></em></div><div class="anim01 photo"></div></div></li>');
-oNodeList[1] = parseDom('<li><div class="content"><div class="ge15"></div><div class="photo anim02 am-animation-slide-left"></div></div></li>');
-oNodeList[2] = parseDom('<li><div class="content"><div class="ge15"></div><div class="photo anim03 am-animation-slide-top"></div></div></li>');
-oNodeList[3] = parseDom('<li><div class="content"><div class="ge15"></div><div class="photo anim04 am-animation-fade"></div></div></li>');
-oNodeList[4] = parseDom('<li><div class="content"><div class="ge25"><h1></h1> <em></em></div><div class="photo anim05 "></div></div></li>');
-oNodeList[5] = parseDom('<li><div class="content"><div class="ge15"></div><div class="photo anim06 model6_1"></div><div class="photo am-animation-slide-left model6_2"></div></div></li>');
-oNodeList[6] = parseDom('<li><div class="content"><div class="ge15"></div><div class="photo am-animation-slide-right model6_1"></div><div class="photo am-animation-slide-bottom model6_2"></div></div></li>');
-oNodeList[7] = parseDom('<li><div class="content"><div class="ge15"></div><div class="photo am-animation-slide-left model8_1"></div><div class="photo am-animation-slide-right model8_2"></div></div></li>');
+oNodeList[0] = parseDom('<li ><div class="content"><div class="parent"><p class="text"></p></div><div class="anim01 photo"></div></div></li>');
+oNodeList[1] = parseDom('<li><div class="content"><div class="parent"><div class="ge15 text"></div></div><div class="photo anim02 am-animation-slide-left"></div></div></li>');
+oNodeList[2] = parseDom('<li><div class="content"><div class="photo anim03 am-animation-slide-top"></div><div class="parent"><p class="text"></p></div></div></li>');
+oNodeList[3] = parseDom('<li><div class="content"><div class="parent"><div class="ge15 text"></div></div><div class="photo anim04 am-animation-fade"></div></div></li>');
+oNodeList[4] = parseDom('<li><div class="content"><div class="ge5"></div><div class="photo anim05 "></div><div class="ge5"></div><div class="parent"><p class="text"></p></div></div></li>');
+oNodeList[5] = parseDom('<li><div class="content"><div class="parent"><div class="ge10 text"></div></div><div class="photo anim06 model6_1"></div><div class="photo am-animation-slide-left model6_2"></div></div></li>');
+oNodeList[6] = parseDom('<li><div class="content"><div class="parent"><div class="ge15 text"></div></div><div class="photo am-animation-slide-right model6_1"></div><div class="photo am-animation-slide-bottom model6_2"></div></div></li>');
+oNodeList[7] = parseDom('<li><div class="content"><div class="parent"><div class="ge15 text"></div></div><div class="photo am-animation-slide-left model8_1"></div><div class="photo am-animation-slide-right model8_2"></div></div></li>');
 
 //var oUl = document.getElementById('ul');
 //for(var i=0; i < oNodeList.length; i++){
--- public/js/03.js
+++ public/js/03.js
@@ -6,14 +6,14 @@ function parseDom(arg){
     return objE.childNodes;
 }
 
-oNodeList[0] = parseDom('<li><div class="content anim01"><div class="photo"><div><h2 class="am-animation-spin">卡通图</h2></div><div class="am-animation-slide-right"><p>照片</p><span> 留&nbsp&nbsp&nbsp住&nbsp&nbsp&nbsp时&nbsp&nbsp&nbsp间&nbsp&nbsp&nbsp带&nbsp&nbsp&nbsp走&nbsp&nbsp&nbsp的</span></div></div></div></li>');
-oNodeList[1] = parseDom('<li class="anim02"><div class="content photo"><div class="heighten"><p>跟着感觉走，纵是初见，</p><p>&nbsp&nbsp蓦然回首，也似曾相识</p></div></div></li>');
-oNodeList[2] = parseDom('<li class="anim03"><div class="content photo"><div class="heighten"><h2>纪念</h2><p>很多时候</p><p>瞬间划过指尖</p><p>留下的就只剩纪念</p></div></div></li>');
-oNodeList[3] = parseDom('<li><div class="content anim04"><div class="photo"></div><div class="text am-animation-slide-right"><h2>一眼万年</h2><p>换个心情，轻装上阵</p><p>风光如许，一眼万年</p></div></div></li>');
-oNodeList[4] = parseDom('<li><div class="content anim05"><div class="text "><h2>如诗如画</h2><p>钟灵毓秀，都是自然的诗篇</p><p>风花雪月，都是风光的画卷</p></div><div class="photo flipInY"></div></div></li>');
+oNodeList[0] = parseDom('<li><div class="content anim01"><div class="photo"><div class="am-animation-slide-right"><p class="text"></p></div></div></div></li>');
+oNodeList[1] = parseDom('<li class="anim02"><div class="content photo"><div class="heighten"><div class="parent"><p class="text"></p></div></div></div></li>');
+oNodeList[2] = parseDom('<li class="anim03"><div class="content photo"><div class="heighten"><div class="parent"><p class="text"></p></div></div></div></li>');
+oNodeList[3] = parseDom('<li><div class="content anim04"><div class="photo"></div><div class="am-animation-slide-right"><div class="parent"><p class="text"></p></div></div></div></li>');
+oNodeList[4] = parseDom('<li><div class="content anim05"><div class="parent "><p class="text"></p></div><div class="photo flipInY"></div></div></li>');
 oNodeList[5] = parseDom('<li><div class="content anim06"><div class="photo am-animation-slide-left"></div><div class="photo am-animation-slide-right"></div><div class="text am-animation-fade"><p>风景的美好，不止是视觉的到达</p><p>更是一种朦胧的期盼</p></div></div></li>');
-oNodeList[6] = parseDom('<li><div class="content anim07"><div class="text"><h2>印象</h2></div><div class="photo heighten"></div><div class="photo widthen"></div><div class="photo widthen"></div><div class="text am-animation-fade"><p>小小的取景框</p><p>小小的印象</p></div></div></li>');
-oNodeList[7] = parseDom('<li><div class="content anim08"><div class="photo am-animation-fade"></div><p></p><div class="photo am-animation-fade"></div></div></li>');
+oNodeList[6] = parseDom('<li><div class="content anim07"><div class="parent"><p class="text"></p></div><div class="photo heighten"></div><div class="photo widthen"></div><div class="photo widthen"></div></li>');
+oNodeList[7] = parseDom('<li><div class="content anim08"><div class="photo am-animation-fade"></div><div class="parent"><p class="text"></p></div><div class="photo am-animation-fade"></div></div></li>');
 
 //var oUl = document.getElementById('ul');
 //for(var i=0; i < oNodeList.length; i++){
